dd6ee2d9564b4dafb6199bdb763ee1ee90a13241
E Mincarelli <sancho.poncho@laposte.net>
2021-03-01 14:09:58 +0100

    creation of the config.py file

---
+++ Classes_Python/config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+print()
+
+""" Categories proposed to users """
+Categories = ('Snacks salés', 'Gâteaux', 'Sodas', 'Desserts glacés', 'Plats préparés')
+
+""" Number of products loaded by category """
+products_nb = 50
+
+""" user of MySQL """
+user = 'root'
+
+""" password of connexion to SQL """
+password = '123ab456'
+
+""" host of connexion to SQL """
+host = 'localhost'
+ 
+""" database used """
+database = 'BDD_OFF'
