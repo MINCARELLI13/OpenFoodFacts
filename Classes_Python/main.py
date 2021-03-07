b5dfba9bb8807a7d6e85f0e5ddd32b1205bddf77
E Mincarelli <sancho.poncho@laposte.net>
2021-03-07 11:43:15 +0100

    main file of project 5

---
+++ Classes_Python/main.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+from Create_Tables_BDD_OFF import Tables_BDD_OFF
+from Filling_BDD_OFF import Filling_of_BDD_OFF
+
+if __name__=='__main__':
+    Begin = Filling_of_BDD_OFF()
+    # Tables = Tables_BDD_OFF()
+    # Tables.drop_all_tables_BDD_OFF()
+    # Tables.create_all_tables_BDD_OFF()
+    # execute('Filling_BDD_OFF.py')
+
