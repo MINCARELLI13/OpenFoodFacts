a40ccea6a5d94b49f9f55606df5ecb3defa1b6da
E Mincarelli <sancho.poncho@laposte.net>
2021-03-07 23:02:09 +0100

    Creation of a menu

--- Classes_Python/menu_OFF.py
+++ Classes_Python/menu_OFF.py
@@ -1,18 +1,42 @@
 # coding: utf-8
-print()
- 
-print('1- Choisir une catégorie de produits')
-print('2- Afficher les produits substitués')
-print('3- Réinitialiser la base de données')
-print('4- Quitter le programme')
+import config
+
 print()
 
-option = str(input('Sélectionnez une action :'))
+def select_category():
+    for i in range(1,6):
+        print(i, '-', config.categories[i-1] )
+    print()
 
-while option not in ['1', '2', '3', '4']:
-    option = str(input('Sélectionnez une action :'))
+    cat = int(input('Sélectionnez une catégorie : '))
+    while cat not in range(1,6):
+        cat = int(input('Sélectionnez une catégorie (1 à 5) : '))
+
+    print()
+    print(cat)
+    print()
 
 
-print('Option...', option)
+
+actions = {
+        '1': 'Choisir une des catégories de produits',
+        '2': 'Afficher les produits substitués',
+        '3': 'Réinitialiser la base de données',
+        '4': 'Quitter le programme'
+        }
+
+[print(key, '- ', actions[key]) for key in actions.keys()]
 print()
+
+action = str(input('Sélectionnez une action : '))
+
+while action not in ['1', '2', '3', '4']:
+    action = str(input('Sélectionnez une action (1 à 4) : '))
+
+print()
+print('--')
+
+# print(actions[action])
+if action == '1':
+    select_category()
 
