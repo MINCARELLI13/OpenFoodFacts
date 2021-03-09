472b2ca59c63fd246d67b9c36160bc22ba151056
E Mincarelli <sancho.poncho@laposte.net>
2021-03-09 23:37:05 +0100

    No change

--- Classes_Python/main.py
+++ Classes_Python/main.py
@@ -1,9 +1,36 @@
 # coding: utf-8
-from Create_Tables_BDD_OFF import Tables_BDD_OFF
-from Filling_BDD_OFF import Filling_of_BDD_OFF
+from create_Tables_BDD_OFF import Tables_BDD_OFF
+from filling_BDD_OFF import Filling_of_BDD_OFF
+from menu_OFF import Menu
+
+
+# class Main:
+""" one program to rule them all """
+
+    
 
 if __name__=='__main__':
-    Begin = Filling_of_BDD_OFF()
+    menu = Menu()
+    # Tables = Tables_BDD_OFF()
+    # response_products = Tables.display_products(5)
+    # for product in response_products:
+    #     print(product)
+    # print()
+    # reinitialisation = Filling_of_BDD_OFF()
+    # menu = Menu()
+    # if menu.action == 1:
+    #     print('Choisir une des catégories de produits')
+    # if menu.action == 2:
+    #     print('Afficher les produits substitués')
+    # if menu.action == 3:
+    #     print('Réinitialiser la base de données')
+    # if menu.action == 4:
+    #     print('Quitter le programme')
+
+    # print(actions[action])
+    # if action == '1':
+    #     select_category()
+    #     Begin = Filling_of_BDD_OFF()
     # Tables = Tables_BDD_OFF()
     # Tables.drop_all_tables_BDD_OFF()
     # Tables.create_all_tables_BDD_OFF()
