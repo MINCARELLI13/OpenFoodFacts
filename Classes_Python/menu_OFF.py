d203b46db0a9b314f9046d7a6f02303ba3d38138
E Mincarelli <sancho.poncho@laposte.net>
2021-03-09 23:38:41 +0100

    Add procedures select_category() and display_products()

--- Classes_Python/menu_OFF.py
+++ Classes_Python/menu_OFF.py
@@ -2,13 +2,15 @@
 import os
 import config
 from filling_BDD_OFF import Filling_of_BDD_OFF
+from create_Tables_BDD_OFF import Tables_BDD_OFF
 
 print()
 
-class Menu:
+class Menu(Tables_BDD_OFF):
     """ offers a menu with 4 possible actions """
 
     def __init__(self):
+        Tables_BDD_OFF.__init__(self)
         # 4 possible actions from 1 to 4
         self.action_chosen = self.select_action()
 
@@ -67,8 +69,51 @@ class Menu:
             print()
             cat = int(input('Sélectionnez une catégorie (1 à 5) : '))
         print()
-        return cat
-    
+        self.display_products(cat)
+        # return cat
+
+    def display_products(self, catg_id):
+        """ displays finded products of choosed category """
+        os.system('CLS')
+        print('Affichage des produits trouvés pour la catégorie "', config.categories[catg_id-1],'" :')
+        print()
+        query = f" SELECT * FROM Product WHERE Product.category_id='{catg_id}' "
+        self.cursor.execute(query)
+        # displaying of products
+        response_products = self.cursor.fetchall()
+        increment = 0
+        for product in response_products:
+            increment += 1
+            print(increment, '-', product)
+        print()
+        choice = int(input("Sélectionnez un produit (ou '0' pour quitter) : "))
+        while choice not in range(0,increment+1):
+            os.system('CLS')
+            print('Affichage des produits trouvés pour la catégorie "', config.categories[catg_id-1],'" :')
+            print()
+            increment = 0
+            for product in response_products:
+                increment += 1
+                print(increment, '-', product)
+            print()
+            choice = int(input("Sélectionnez un produit (ou '0' pour quitter) : "))
+        print()
+        if choice == 0:
+            print('Arrêt du programme demandé...')
+            print()
+            quit()
+        else:
+            os.system('CLS')
+            print('Affichage du produit sélectionné pour la catégorie "', config.categories[catg_id-1],'" :')
+            print()
+            for i in range(1,7):
+                print('  ',config.name_of_product_fields[i-1], ' :', response_products[choice-1][i])
+            print()
+            
+
+
+
+
     def display_substituts(self):
         """ displays the recorded substituts """
         print('     def display_substituts()')
